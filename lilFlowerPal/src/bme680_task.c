35c6cfb0496b92b7bc4b76df2cf580d11df913e8
Marcel Nahir Samur <mnsamur2014@gmail.com>
2024-06-09 23:48:34 -0300

    Now using UncleRus BME680 driver

--- lilFlowerPal/src/bme680_task.c
+++ lilFlowerPal/src/bme680_task.c
@@ -8,8 +8,12 @@
  * @copyright Copyright (c) 2024
  *
  */
-#include "bme680.h"
-#include "esp8266_wrapper.h"
+#include <bme680.h>
+#include <esp_system.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "bme680_task.h"
 #include "main_tasks_common.h"
@@ -19,8 +23,7 @@
 #define I2C_SCL_PIN 22
 #define I2C_SDA_PIN 21
 #define I2C_FREQ    I2C_FREQ_100K
-
-static bme680_sensor_t *sensor;
+#define I2C_ADDR    BME680_I2C_ADDR_0
 
 /**
  * @brief task that triggers measurements of sensor every seconds. It uses
@@ -31,71 +34,53 @@ static bme680_sensor_t *sensor;
  */
 static void bme680_task(void *pvParameters)
 {
-    bme680_values_float_t values;
+    bme680_t sensor;
+    memset(&sensor, 0, sizeof(bme680_t));
+
+    ESP_ERROR_CHECK(bme680_init_desc(&sensor, I2C_ADDR, I2C_BUS, I2C_SDA_PIN, I2C_SCL_PIN));
+
+    // init the sensor
+    ESP_ERROR_CHECK(bme680_init_sensor(&sensor));
+
+    // Changes the oversampling rates to 4x oversampling for temperature
+    // and 2x oversampling for humidity. Pressure measurement is skipped.
+    bme680_set_oversampling_rates(&sensor, BME680_OSR_4X, BME680_OSR_NONE, BME680_OSR_2X);
+
+    // Change the IIR filter size for temperature and pressure to 7.
+    bme680_set_filter_size(&sensor, BME680_IIR_SIZE_7);
+
+    // Change the heater profile 0 to 200 degree Celsius for 100 ms.
+    bme680_set_heater_profile(&sensor, 0, 200, 100);
+    bme680_use_heater_profile(&sensor, 0);
+
+    // Set ambient temperature to 10 degree Celsius
+    bme680_set_ambient_temperature(&sensor, 10);
+
+    // as long as sensor configuration isn't changed, duration is constant
+    uint32_t duration;
+    bme680_get_measurement_duration(&sensor, &duration);
 
     TickType_t last_wakeup = xTaskGetTickCount();
 
-    // as long as sensor configuration isn't changed, duration is constant
-    uint32_t duration = bme680_get_measurement_duration(sensor);
-
+    bme680_values_float_t values;
     while (1) {
         // trigger the sensor to start one TPHG measurement cycle
-        if (bme680_force_measurement(sensor)) {
+        if (bme680_force_measurement(&sensor) == ESP_OK) {
             // passive waiting until measurement results are available
             vTaskDelay(duration);
 
-            // alternatively: busy waiting until measurement results are available
-            // while (bme680_is_measuring (sensor)) ;
-
             // get the results and do something with them
-            if (bme680_get_results_float(sensor, &values))
-                printf("%.3f BME680 Sensor: %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm\n",
-                       (double)sdk_system_get_time() * 1e-3,
-                       values.temperature, values.humidity,
-                       values.pressure, values.gas_resistance);
+            if (bme680_get_results_float(&sensor, &values) == ESP_OK)
+                printf("BME680 Sensor: %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm\n",
+                       values.temperature, values.humidity, values.pressure, values.gas_resistance);
         }
         // passive waiting until 1 second is over
-        vTaskDelayUntil(&last_wakeup, 1000 / portTICK_PERIOD_MS);
+        vTaskDelayUntil(&last_wakeup, pdMS_TO_TICKS(1000));
     }
 }
 
 void bme680_task_start(void)
 {
-    // Set UART Parameter.
-    uart_set_baud(0, 115200);
-    // Give the UART some time to settle
-    vTaskDelay(1);
-
-    // Init all I2C bus interfaces at which BME680 sensors are connected
-    i2c_init(I2C_BUS, I2C_SCL_PIN, I2C_SDA_PIN, I2C_FREQ);
-
-    // Init the sensor with slave address BME680_I2C_ADDRESS_1 connected to I2C_BUS.
-    sensor = bme680_init_sensor(I2C_BUS, BME680_I2C_ADDRESS_1, 0);
-
-    if (sensor) {
-        /** -- SENSOR CONFIGURATION PART (optional) --- */
-
-        // Changes the oversampling rates to 4x oversampling for temperature
-        // and 2x oversampling for humidity. Pressure measurement is skipped.
-        bme680_set_oversampling_rates(sensor, osr_4x, osr_none, osr_2x);
-
-        // Change the IIR filter size for temperature and pressure to 7.
-        bme680_set_filter_size(sensor, iir_size_7);
-
-        // Change the heater profile 0 to 200 degree Celcius for 100 ms.
-        bme680_set_heater_profile(sensor, 0, 200, 100);
-        bme680_use_heater_profile(sensor, 0);
-
-        // Set ambient temperature to 10 degree Celsius
-        bme680_set_ambient_temperature(sensor, 10);
-
-        /** -- TASK CREATION PART --- */
-
-        // must be done last to avoid concurrency situations with the sensor
-        // configuration part
-
-        // Create a task that uses the sensor
-        xTaskCreatePinnedToCore(&bme680_task, "bme680_task", BME680_TASK_STACK_SIZE, NULL, BME680_TASK_PRIORITY, NULL, BME680_TASK_CORE_ID);
-    } else
-        printf("Could not initialize BME680 sensor\n");
+    ESP_ERROR_CHECK(i2cdev_init());
+    xTaskCreatePinnedToCore(&bme680_task, "bme680_task", BME680_TASK_STACK_SIZE, NULL, BME680_TASK_PRIORITY, NULL, BME680_TASK_CORE_ID);
 }
--- lilFlowerPal/tasks/main_tasks_common.h
+++ lilFlowerPal/tasks/main_tasks_common.h
@@ -2,9 +2,9 @@
 #define MAIN_TASKS_COMMON_H_
 
 // LED blink task
-#define BME680_TASK_STACK_SIZE 2048
-#define BME680_TASK_PRIORITY   4
-#define BME680_TASK_CORE_ID    0
+#define BME680_TASK_STACK_SIZE 6144
+#define BME680_TASK_PRIORITY   3
+#define BME680_TASK_CORE_ID    1
 
 // Encoder task
 #define ENCODER_TASK_STACK_SIZE 2048
